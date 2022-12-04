734918d20f6482b3889cdf6caecd9d5f44bbe5a8
Will Wang <will.wang@fullscript.com>
2022-12-03 23:03:22 -0500

    refactor day 3

--- day03/p1.rb
+++ day03/p1.rb
@@ -1,28 +1,17 @@
 file = File.open 'input.txt'
 data = file.readlines.map(&:chomp)
 
-
-letters = "abcdefghijklmnopqrstuvwxyz"
-letters += letters.upcase
 mapping = {}
-letters.split('').each_with_index do |l, i|
-  mapping[l] = i+1
-end
+('a'..'z').each_with_index { |l, i| mapping[l] = i + 1 }
+('A'..'Z').each_with_index { |l, i| mapping[l] = i + 27 }
 
 sum = 0
 data.each do |row|
-  c1 = row[0, row.size/2].split('').map { |l| mapping[l] }.sort
-  c2 = row[row.size/2, row.size/2].split('').map { |l| mapping[l] }.sort
-  i, j = 0, 0
-  while i < c1.length && j < c2.length
-    if c1[i] < c2[j]
-      i += 1
-    elsif c1[i] > c2[j]
-      j += 1
-    else
-      sum += c1[i]
-      break
-    end
+  c1 = row[0, row.size/2].split('').map { |l| mapping[l] }
+  c2 = row[row.size/2, row.size/2].split('').map { |l| mapping[l] }
+  t = c1 & c2 # or .intersection
+  unless t.empty?
+    sum += t.uniq.sum
   end
 end
 
