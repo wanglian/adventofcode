8826d3b4f7f4e199cfb5709d32990bf76fd93714
Will Wang <will.wang@fullscript.com>
2022-12-04 11:05:41 -0500

    refactor

--- day04.rb
+++ day04.rb
@@ -1,26 +1,20 @@
-file = File.open 'day04.txt'
-data = file.readlines.map(&:chomp).map { |r| r.split(',').map { |g| g.split('-').map(&:to_i) } }
+data = File.open('day04.txt').readlines.map(&:chomp)
+  .map { |r| r.split(',').map { |g| g.split('-').map(&:to_i) } }
 
 def p1(data)
-  re = 0
-  data.each do |row|
-    if (row[0][0] <= row[1][0] && row[0][1] >= row[1][1]) ||
-       (row[0][0] >= row[1][0] && row[0][1] <= row[1][1])
-      re += 1
-    end
+  data.inject(0) do |sum, row|
+    (row[0][0] <= row[1][0] && row[0][1] >= row[1][1]) ||
+    (row[0][0] >= row[1][0] && row[0][1] <= row[1][1]) ?
+      sum + 1 : sum
   end
-  re
 end
 
 def p2(data)
-  re = 0
-  data.each do |row|
-    if (row[0][0] <= row[1][0] && row[0][1] >= row[1][0]) ||
-       (row[0][0] >= row[1][0] && row[0][0] <= row[1][1])
-      re += 1
-    end
+  data.inject(0) do |sum, row|
+    (row[0][0] <= row[1][0] && row[0][1] >= row[1][0]) ||
+    (row[0][0] >= row[1][0] && row[0][0] <= row[1][1]) ?
+      sum + 1 : sum
   end
-  re
 end
 
 p p1(data)
