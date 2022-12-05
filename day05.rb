d5cedf9dab2389ffcaefb08056fe958fdd92343e
Will Wang <will.wang@fullscript.com>
2022-12-05 12:08:42 -0500

    refactor

--- day05.rb
+++ day05.rb
@@ -3,23 +3,34 @@ data = File.open('input/day05.txt').readlines.map(&:chomp)
 def parse_stacks(data)
   stacks = []
   n = 0
-  while data[n][1, 1] != '1' ## [Z] [M] [P]
-    i = 0
-    while (k = (1 + i*4)) < data[n].length
-      c = data[n][k, 1]
-      unless c == ' '
-        stacks[i+1] ||= [] # start from 1
-        stacks[i+1].unshift data[n][k, 1]
-      end
-      i += 1
+  while data[n][1, 1] != '1'
+    ## [Z] [M] [P]
+    # regex
+    matches = data[n].to_enum(:scan, /[[A-Z]]/).map { Regexp.last_match }
+    matches.each do |match|
+      pos = match.begin(0)/4 + 1 # start from 1
+      stacks[pos] ||= []
+      stacks[pos].unshift match.to_s
     end
+    # manual match
+    # i = 0
+    # while (k = (1 + i*4)) < data[n].length
+    #   c = data[n][k, 1]
+    #   unless c == ' '
+    #     stacks[i+1] ||= [] # start from 1
+    #     stacks[i+1].unshift data[n][k, 1]
+    #   end
+    #   i += 1
+    # end
     n += 1
   end
+
   [stacks, n + 2] # stacks, and cursor of operations
 end
 
 def parse_operation(row)
-  row.scan(/\d+/).map(&:to_i) ## move 1 from 2 to 1
+  ## move 1 from 2 to 1
+  row.scan(/\d+/).map(&:to_i)
 end
 
 def peek(stacks)
