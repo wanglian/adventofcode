594107d4ff879a11645e047f28912303b703bf03
Will Wang <will.wang@fullscript.com>
2022-12-05 00:56:58 -0500

    day 5

---
+++ day05.rb
@@ -0,0 +1,57 @@
+data = File.open('input/day05.txt').readlines.map(&:chomp)
+
+def parse_stacks(data)
+  stacks = []
+  n = 0
+  while data[n][1, 1] != '1' ## [Z] [M] [P]
+    i = 0
+    while (k = (1 + i*4)) < data[n].length
+      c = data[n][k, 1]
+      unless c == ' '
+        stacks[i+1] ||= [] # start from 1
+        stacks[i+1].unshift data[n][k, 1]
+      end
+      i += 1
+    end
+    n += 1
+  end
+  [stacks, n + 2] # stacks, and cursor of operations
+end
+
+def parse_operation(row)
+  row.scan(/\d+/).map(&:to_i) ## move 1 from 2 to 1
+end
+
+def peek(stacks)
+  stacks.inject("") do |sum, stack|
+    sum += stack.last if stack&.last
+    sum
+  end
+end
+
+def p1(data)
+  stacks, n = parse_stacks(data)
+  while n < data.length
+    count, from, to = parse_operation(data[n])
+    count.times { stacks[to].push(stacks[from].pop) }
+    n += 1
+  end
+
+  peek(stacks)
+end
+
+def p2(data)
+  stacks, n = parse_stacks(data)
+  while n < data.length
+    count, from, to = parse_operation(data[n])
+    tmp = []
+    count.times { tmp.push(stacks[from].pop) }
+    count.times { stacks[to].push(tmp.pop) }
+    n += 1
+  end
+
+  peek(stacks)
+end
+
+p p1(data)
+p p2(data)
