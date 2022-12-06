5401692c1bd8ab1c7e31af784afa3a8b9c5c1623
Will Wang <will.wang@fullscript.com>
2022-12-06 10:49:50 -0500

    debug

--- day06.rb
+++ day06.rb
@@ -1,13 +1,12 @@
 data = File.open('input/day06.txt').readlines.map(&:chomp)[0]
+# binding.irb ## debug for ruby > 3
 
 def detect(data, n)
   arr = []
   data.each_char.with_index do |c, index|
     arr.push(c)
     arr.shift if arr.length > n
-    if arr.uniq.length == n
-      return index + 1
-    end
+    return index + 1 if arr.uniq.length == n
   end
   raise "not found"
 end
