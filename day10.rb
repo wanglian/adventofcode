550af79cd2cd77deb0d11bc43a86390699f79e3e
Will Wang <will.wang@fullscript.com>
2022-12-10 11:31:00 -0500

    refactor

--- day10.rb
+++ day10.rb
@@ -1,30 +1,20 @@
 data = File.open('input/day10.txt').readlines.map(&:chomp)
   .map { |row| row.split(' ') }
 
-def check(cycle, register)
-  # p "#{cycle}: #{register}"
-  if [20, 60, 100, 140, 180, 220].include?(cycle)
-    cycle * register
-  else
-    0
-  end
-end
-
-def p1(data)
+def execute(data, result)
   register = 1
   cycle = 0
-  result = 0
 
   data.each do |row|
     case row[0]
     when 'noop'
       cycle += 1
-      result += check(cycle, register)
+      result += yield(cycle, register)
     when 'addx'
       cycle += 1
-      result += check(cycle, register)
+      result += yield(cycle, register)
       cycle += 1
-      result += check(cycle, register)
+      result += yield(cycle, register)
       register += row[1].to_i
     else
       raise "error: #{row}"
@@ -34,33 +24,22 @@ def p1(data)
   result
 end
 
-def draw(cycle, register)
-  (cycle % 40 - 1 - register).abs > 1 ? '.' : '#'
+def p1(data)
+  execute(data, 0) do |cycle, register|
+    if [20, 60, 100, 140, 180, 220].include?(cycle)
+      cycle * register
+    else
+      0
+    end
+  end
 end
 
 def p2(data)
-  register = 1 # pos
-  cycle = 0 # draw
-  result = ""
-
-  data.each do |row|
-    case row[0]
-    when 'noop'
-      cycle += 1
-      result += draw(cycle, register)
-    when 'addx'
-      cycle += 1
-      result += draw(cycle, register)
-      cycle += 1
-      result += draw(cycle, register)
-      register += row[1].to_i
-    else
-      raise "error: #{row}"
-    end
+  result = execute(data, '') do |cycle, register|
+    (cycle % 40 - 1 - register).abs > 1 ? '.' : '#'
   end
-
-  result.scan(/.{40}/).each { |row| p row }
+  result.scan(/.{40}/)
 end
 
 p p1(data)
-p2(data)
+p2(data).each { |row| p row }
