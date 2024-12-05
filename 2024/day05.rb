20f39459fc7985f348edb67d99a2a8911d9ca05a
Will Wang <will.wang@fullscript.com>
2024-12-05 18:45:16 -0500

    day 05

---
+++ 2024/day05.rb
@@ -0,0 +1,93 @@
+require './utils.rb'
+
+data = get_input("05")
+
+def p1(data)
+  # binding.break
+  rules = []
+  pages = []
+
+  target = rules
+  data.each do |row|
+    if row.empty?
+      target = pages
+    else
+      target << row
+    end
+  end
+
+  rules = rules.map { |rule| rule.split("|").map(&:to_i) }
+  pages = pages.map { |page| page.split(",").map(&:to_i) }
+
+  pages.each.inject(0) do |sum, numbers|
+    sum + if valid_pages?(numbers, rules)
+      numbers[numbers.size/2]
+    else
+      0
+    end
+  end
+end
+
+def valid_pages?(numbers, rules)
+  numbers.each_with_index do |m, i|
+    j = i+1
+    while j < numbers.size
+      return false unless valid_page?(m, numbers[j], rules)
+      j += 1
+    end
+  end
+  true
+end
+
+def valid_page?(m, n, rules)
+  rules.each do |rule|
+    if rule.include?(m) && rule.include?(n)
+      return m == rule[0]
+    end
+  end
+  true
+end
+
+def p2(data)
+  rules = []
+  pages = []
+
+  target = rules
+  data.each do |row|
+    if row.empty?
+      target = pages
+    else
+      target << row
+    end
+  end
+
+  rules = rules.map { |rule| rule.split("|").map(&:to_i) }
+  pages = pages.map { |page| page.split(",").map(&:to_i) }
+
+  pages.each.inject(0) do |sum, numbers|
+    sum + if valid_pages?(numbers, rules)
+      0
+    else
+      numbers = correct(numbers, rules)
+      numbers[numbers.size/2]
+    end
+  end
+end
+
+def correct(numbers, rules)
+  numbers.each_with_index do |m, i|
+    j = i+1
+    while j < numbers.size
+      if valid_page?(m, numbers[j], rules)
+        j += 1
+      else
+        numbers[i], numbers[j] = numbers[j], numbers[i]
+        m = numbers[i]
+        j = i+1
+      end
+    end
+  end
+end
+
+p p1(data)
+p p2(data)
