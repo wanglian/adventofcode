1ad4440354e7d954798a20752e869ef458e2e1ba
Will Wang <will.wang@fullscript.com>
2024-12-12 16:46:05 -0500

    refactor

--- 2024/day12.rb
+++ 2024/day12.rb
@@ -2,34 +2,28 @@ require './utils.rb'
 
 data = get_input("12").map { |row| row.split('') }
 
-def p1(data)
-  # binding.break
-  regions = {}
-  plot_regions = {}
-  mi, mj = data.size-1, data[0].size-1
-  k = 1
+def calc_price(data)
+  plot_regions = {} # [x, y] => "A1"
+  regions = {} # "A1" => [area, perimeter]
+  k = 1 # region sequence no
   data.each_with_index do |row, i|
     row.each_with_index do |v, j|
       region = nil
-      n = 0 # neiboughers
       if i > 0
         if data[i-1][j] == v
           region = plot_regions[[i-1, j]]
-          n += 1
         end
       end
       if j > 0
         if data[i][j-1] == v
           region2 = plot_regions[[i, j-1]]
           if region && region2 && region != region2
-            # p "#{region} - #{region2}"
             # merge
             region2_plots = plot_regions.each { |k, v| plot_regions[k] = region if v == region2 }
             regions[region] = merge_regions(regions[region], regions[region2])
             regions.delete(region2)
           end
           region ||= region2
-          n += 1
         end
       end
       unless region
@@ -37,82 +31,55 @@ def p1(data)
         k += 1
       end
       plot_regions[[i, j]] = region
-      regions[region] = cal_region(regions[region], n)
+
+      regions[region] = yield data, i, j, v, regions[region]
     end
   end
   # p regions
   regions.values.inject(0) { |sum, r| sum + r[0] * r[1] }
-end
-
-def cal_region(region, n)
-  area, perimeter = region || [0, 0]
-  area += 1
-  case n
-  when 0
-    perimeter += 4
-  when 1
-    perimeter += 2
-  end
-  [area, perimeter]
 end
 
 def merge_regions(region1, region2)
   [region1[0] + region2[0], region1[1] + region2[1]]
 end
 
-def p2(data)
-  regions = {}
-  plot_regions = {}
-  mi, mj = data.size-1, data[0].size-1
-  k = 1
-  data.each_with_index do |row, i|
-    row.each_with_index do |v, j|
-      region = nil
-      p1, p2, p3, p4 = false, false, false, false # neiboughers
-      if i > 0
-        if data[i-1][j] == v
-          p3 = true
-          region = plot_regions[[i-1, j]]
-        end
-        if j > 0
-          if data[i-1][j-1] == v
-            p2 = true
-          end
-        end
-        if j < mj
-          if data[i-1][j+1] == v
-            p4 = true
-          end
-        end
-      end
-      if j > 0
-        if data[i][j-1] == v
-          p1 = true
-          region2 = plot_regions[[i, j-1]]
-          if region && region2 && region != region2
-            # merge
-            region2_plots = plot_regions.each { |k, v| plot_regions[k] = region if v == region2 }
-            regions[region] = merge_regions(regions[region], regions[region2])
-            regions.delete(region2)
-          end
-          region ||= region2
-        end
-      end
-      unless region
-        region = "#{v}#{k}"
-        k += 1
-      end
-      plot_regions[[i, j]] = region
-      regions[region] = cal_region2(regions[region], p1, p2, p3, p4)
-    end
+def cal_region(region, p1, p2)
+  area, perimeter = region || [0, 0]
+
+  area += 1
+
+  if p1 && p2
+    # no change
+  elsif p1 || p2
+    perimeter += 2
+  else
+    perimeter += 4
   end
-  # p regions
-  regions.values.inject(0) { |sum, r| sum + r[0] * r[1] }
+
+  [area, perimeter]
+end
+
+def p1(data)
+  calc_price(data) do |data, i, j, v, region|
+    #    p2
+    # p1 []
+    p1, p2 = false, false
+    if i > 0 &&  data[i-1][j] == v
+      p2 = true
+    end
+    if j > 0 && data[i][j-1] == v
+      p1 = true
+    end
+
+    cal_region(region, p1, p2)
+  end
 end
 
 def cal_region2(region, p1, p2, p3, p4)
   area, perimeter = region || [0, 0]
+
   area += 1
+
   if p1
     if p3
       perimeter -= 2 unless p4
@@ -132,6 +99,36 @@ def cal_region2(region, p1, p2, p3, p4)
   end
 
   [area, perimeter]
+end
+
+def p2(data)
+  calc_price(data) do |data, i, j, v, region|
+    # p2 p3 p4
+    # p1 []
+    p1, p2, p3, p4 = false, false, false, false # neighbors
+    if i > 0
+      if data[i-1][j] == v
+        p3 = true
+      end
+      if j > 0
+        if data[i-1][j-1] == v
+          p2 = true
+        end
+      end
+      if j < data[0].size-1
+        if data[i-1][j+1] == v
+          p4 = true
+        end
+      end
+    end
+    if j > 0
+      if data[i][j-1] == v
+        p1 = true
+      end
+    end
+
+    cal_region2(region, p1, p2, p3, p4)
+  end
 end
 
 p "Problem 1:"
