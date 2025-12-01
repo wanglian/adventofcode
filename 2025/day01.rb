44f000130859a79d1d1c49af4c597773f1a04857
Will Wang <will.wang@fullscript.com>
2025-12-01 11:57:29 -0500

    day 01 p2

--- 2025/day01.rb
+++ 2025/day01.rb
@@ -1,25 +1,44 @@
 require './utils.rb'
 
-data = get_input("01")
+data = get_input("01").map do |d|
+  [d[0], d[1..].to_i]
+end
 
 def p1(data)
   # binding.break
   c = 50
-  data.map do |d|
-    [d[0], d[1..].to_i]
-  end.inject(0) do |sum, move|
+  data.inject(0) do |sum, move|
     direction, n = move
     if direction == 'R'
-      c = (c + n) % 100
+      c += n
     else
-      c = (c - n + 100) % 100
+      c -= n
     end
+    c %= 100
     sum + (c == 0 ? 1 : 0)
   end
 end
 
 def p2(data)
-
+  c = 50
+  data.inject(0) do |sum, move|
+    direction, n = move
+    z = 0
+    if direction == 'R'
+      c += n
+      z = c / 100
+      c %= 100
+    else
+      if c == 0
+        c += 100
+      end
+      c -= n
+      z = - (c / 100)
+      c %= 100
+      z += 1 if c == 0
+    end
+    sum + z
+  end
 end
 
 p "Problem 1:"
