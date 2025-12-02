67fdadf50f7ff91a90f38cfe252eae6917827d2a
Will Wang <will.wang@fullscript.com>
2025-12-02 15:27:33 -0500

    p2

--- 2025/day02.rb
+++ 2025/day02.rb
@@ -19,6 +19,32 @@ def invalid?(n)
   s[0, h] == s[h, h]
 end
 
+def check2(range)
+  min, max = range
+  (min..max).inject(0) do |sum, n|
+    is_invalid = invalid2?(n)
+    sum + (is_invalid ? n : 0)
+  end
+end
+
+# TODO: improve performance
+def invalid2?(n)
+  s = n.to_s
+  size = s.size
+  k = size / 2
+
+  (1..k).each do |i|
+    return true if repeat_n?(s, i)
+  end
+  false
+end
+
+def repeat_n?(s, n)
+  return false unless s.size % n == 0
+
+  s.scan(/.{1,#{n}}/).uniq.size == 1
+end
+
 def p1(data)
   # binding.break
   data.inject(0) do |sum, range|
@@ -27,7 +53,9 @@ def p1(data)
 end
 
 def p2(data)
-
+  data.inject(0) do |sum, range|
+    sum + check2(range)
+  end
 end
 
 p "Problem 1:"
