af2ac28fe312cb7ccd9e21d6799302feb833d4bc
Will Wang <will.wang@fullscript.com>
2025-12-03 18:46:13 -0500

    p2

--- 2025/day03.rb
+++ 2025/day03.rb
@@ -7,20 +7,30 @@ end
 def p1(data)
   # binding.break
   data.inject(0) do |sum, row|
-    b1 = row.max
-    i = row.index(b1) + 1
-    if i == row.size
-      b2 = row.pop
-      b1 = row.max
-    else
-      b2 = row[i..].max
-    end
-    sum + "#{b1}#{b2}".to_i
+    sum + check(row)
   end
 end
 
-def p2(data)
+def check(row, m=2)
+  result = []
+  n = 0
+  i = 0
+  s = row.size
+  while n < m
+    j = s - m + n
+    a = row[i..j]
+    r = a.max
+    i += a.index(r)+1
+    result << r
+    n += 1
+  end
+  result.join.to_i
+end
 
+def p2(data)
+  data.inject(0) do |sum, row|
+    sum + check(row, 12)
+  end
 end
 
 p "Problem 1:"
